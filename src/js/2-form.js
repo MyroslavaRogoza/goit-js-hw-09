b159160cc602c3ef774ea736bada3765ff8b69e9
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-01-26 14:06:48 +0200

    'update'

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,62 @@
 const form = document.querySelector('.feedback-form');
 const input = form.querySelector("input");
-// input.setAttribute("placeholder", "Type area");
+input.setAttribute("required", 'true');
+const textarea = form.querySelector('textarea');
+textarea.setAttribute("required", 'true');
+const LSKey = "feedback-form-state";
+
+function saveToLS(key, value){
+    const valueJson = JSON.stringify(value);
+    localStorage.setItem(key, valueJson);
+}
+
+function loadFromLS(key){
+    const data = localStorage.getItem(key);
+    try {
+    const result = JSON.parse(data);
+    return result;
+    }catch{
+    return data;
+    }
+ }
+
+ form.addEventListener('input', ()=>{
+
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value.trim();
+
+    const userData = {
+        email: userEmail,
+        message: userMessage,
+    };
+
+    saveToLS(LSKey, userData);
+},);
+
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const data = loadFromLS(LSKey);
+    console.log(data);
+
+    localStorage.removeItem(LSKey);
+
+    form.reset();
+},);
+
+input.addEventListener('focus', ()=>{
+    input.setAttribute('placeholder', 'Type area');
+},);
+
+input.addEventListener('blur', ()=>{
+    input.setAttribute('placeholder', '');
+},);
+
+function restoreUserData(){
+    const userData =  loadFromLS(LSKey);
+
+    input.value = userData.email ?? '';
+    textarea.value = userData.message ?? '';
+}
+restoreUserData();
+
